2d05d3574656187ce1f7c44bb1da311e8786b0e7
Ken Svensson <ken@ceronus.com>
2021-12-28 16:53:33 +0700

    added some cosmetic changes to the interfaces for documentation

--- src/IMedius.cs
+++ src/IMedius.cs
@@ -5,8 +5,28 @@ namespace Medius
 {
     public interface IMedius
     {
+        /// <summary>
+        /// Invoke the associated <see cref="MediusCommandHandler{IMediusCommand}"/>.
+        /// </summary>
+        /// <param name="command">The <see cref="IMediusCommand"/> that will be injected to the handler.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that will be assigned to the new task.</param>
+        /// <returns>The associated handler's <see cref="Task"/>.</returns>
         Task InvokeAsync(IMediusCommand command, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Invoke the associated <see cref="MediusQueryHandler{TQuery, IMediusQuery{TQueryResult}}"/>.
+        /// </summary>
+        /// <param name="query">The <see cref="IMediusQuery{TQueryResult}"/> that will be injected to the associated handler.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that will be assigned to the new task.</param>
+        /// <returns>The associated handler's <see cref="Task"/>.</returns>
         Task<TQueryResult> InvokeAsync<TQueryResult>(IMediusQuery<TQueryResult> query, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Invoke the associated <see cref="MediusActionHandler{IMediusAction{TActionResult}, TActionResult}"/>.
+        /// </summary>
+        /// <param name="action">The <see cref="IMediusAction{TActionResult}"/> that will be injected to the associated handler.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that will be assigned to the new task.</param>
+        /// <returns>The associated handler's <see cref="Task"/>.</returns>
         Task<TActionResult> InvokeAsync<TActionResult>(IMediusAction<TActionResult> action, CancellationToken cancellationToken = default);
     }
 }
--- src/IMediusBaseHandler.cs
+++ src/IMediusBaseHandler.cs
@@ -5,6 +5,12 @@ namespace Medius
 {
     public interface IMediusBaseHandler<TOperation, TOperationResult>
     {
+        /// <summary>
+        /// Handle the injected operation (Action, Command or Query).
+        /// </summary>
+        /// <param name="operation">The injected Action, Command or Query.</param>
+        /// <param name="cancellationToken">The <see cref="CancellationToken"/> that will be assigned to the new task.</param>
+        /// <returns></returns>
         Task<TOperationResult> HandleAsync(TOperation operation, CancellationToken cancellationToken);
     }
 }
